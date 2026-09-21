72d6eba11e1e9cadd76ab86443591b1f4315033e
Shyraz-Referee <shyraz95@yahoo.fr>
2026-09-21 22:48:30 +0200

    Change webDir path to current directory

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'fr.roc.houilles.plateaurugby',
   appName: 'Plateau Rugby ROC',
-  webDir: 'www',
+  webDir: '.',
   server: { androidScheme: 'https' },
   android: { allowMixedContent: false }
 };
